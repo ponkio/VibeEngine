ebdcea1905e6e56a84f77ebc42fac3d0b2c40a10
C0ntra99 <efowler@contra.icu>
2020-01-07 20:36:24 -0500

    Started working on the config command. Need to think through how I want to pass the submodules the config

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,4 +1,8 @@
 const {Command, flags} = require('@oclif/command')
+const prompt = require('prompt')
+const colors = require('colors/safe')
+const fs = require('fs')
+
 //VibeEngine config -m {Server|Client|Standalone}
 //Walks user through the setup process for whichever mode and will spit out a json file
 //Json file is the config for the client and server, 
@@ -31,26 +35,64 @@ For standalone configuration both client and server will be populated
 */
 class ConfigCommand extends Command {
     async run(){
+        let schema = {
+            properties:{
+                name1:{
+                    description: "Enter name1 pls:",
+                    type:'string',
+                    default:'name1'
+                },
+                name2:{
+                    description: "Enter name2:",
+                    type:'string',
+                    default:'name2'
+                }
+            }
+        }
         const {flags} = this.parse(ConfigCommand)
 
-        this.log(`CONFIG: ${flags.interactive}`)
+        if(flags.interactive){
+            prompt.message=colors.green(`VibeEngine Config:${flags.mode}`)
+            prompt.start()
+            prompt.get(schema, (err, res) => {
+                if(err) {
+                    console.log(err)
+                }
+
+            })
+        } else if(!flags.config) {
+            this.error("-c, --config= is required with the --no-interactive flag", {exit:1})
+        }
+        //this.log(`CONFIG: ${flags.interactive}`)
     }
 }
 
 ConfigCommand.description = `Configure VibeEngine components`
 
+ConfigCommand.examples = [
+    '$ VibeEngine config -m client',
+    '$ VibeEngine config -m server',
+    '$ VibeEngine config --no-interactive -c /tmp/client_config.conf'
+]
 ConfigCommand.flags = {
     interactive:flags.boolean({
-        char:'i',
+        //char:'i',
         default:true,
-        description:'Generate the configuration file based on user input',
+        description:'Setting to false will skip the interactive walkthrough and read the config file that was passed.',
         hidden:true,
         allowNo:true
     }),
     config:flags.string({
         char:'c',
         description:'Path to the configuration file',
-        default:'VibeEngine.conf',
+        dependsOn:['interactive']
+        //default:'VibeEngine.conf'
+    }),
+    mode:flags.string({
+        char:'m',
+        description:'VibeEgine mode',
+        options:['client', 'server', 'standalone'],
+        required:true
     })
 }
 
